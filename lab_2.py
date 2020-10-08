e9d3e64b331573094f7be911a7844b95e603d9ad
araag2 <artur.guimas@gmail.com>
2020-10-08 16:50:28 +0100

    basic structure

--- lab_2.py
+++ lab_2.py
@@ -2,4 +2,12 @@ import sklearn
 import spacy
 import nltk
 import whoosh
+import os
+import glob
 
+def inverted_index(directory):
+    os.chdir(directory)
+    files = glob.glob('*.txt')
+    print(files)
+
+inverted_index('lab2_material')
