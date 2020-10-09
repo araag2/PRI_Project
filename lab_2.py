043345cf5d46a39a0b21ec95a5e97777d20ce28e
araag2 <artur.guimas@gmail.com>
2020-10-09 11:49:11 +0100

    Lab 2 alterations

--- lab_2.py
+++ lab_2.py
@@ -4,6 +4,7 @@ import nltk
 import whoosh
 import os
 import glob
+import matplotlib.pyplot as plt
 
 ## Exercise 4.1 from Lab1
 def inverted_index(dic):
@@ -11,42 +12,49 @@ def inverted_index(dic):
     files = glob.glob('*.txt')
     
     index = {}
-    doc_index = 0
-        
+    auxiliary_map = {}
+    index['document_count'] = 0
+
     for _ in files:
-        doc_index += 1
+        index['document_count'] += 1
         with open(_, 'r') as f:
+            auxiliary_map[index['document_count']] = []
+
             words = nltk.word_tokenize(f.read())
             word_vector = nltk.FreqDist(words)
+            token_count = 0
+            term_count = 0
             for item in word_vector.items():
                 if item[0] in index:
                     index[item[0]][0] += 1
-                    index[item[0]][1] += [(doc_index, item[1]), ]
-
-                if item[0] not in index:
-                    index[item[0]] = [0, []]
-                    index[item[0]][0] = 1
-                    index[item[0]][1] = [(doc_index, item[1])]
+                    index[item[0]] += [(index['document_count'], item[1]), ]
+                else:
+                    index[item[0]] = [1,]
+                    index[item[0]] += [(index['document_count'], item[1]), ]
+                token_count += item[1]
+                term_count += 1
+            auxiliary_map[index['document_count']] = (token_count,term_count)
     print(index)
-    return index
+    print(auxiliary_map)
+    return index, auxiliary_map
 
 ## Exercise 4.2 from Lab1
 def statistics(index):
-    n_seen_id = []
-    n_documents = 0
     n_terms = len(index.keys())
     n_occurences = 0
 
     for term_i in index:
-        for term in index[term_i][1]:
-            if term[0] not in n_seen_id:
-                n_seen_id += [term[0], ]
-                n_documents += 1
-            n_occurences += term[1]
+        if term_i != 'document_count':
+            for term in index[term_i][1:]:
+                n_occurences += term[1]
                     
-    print("Number of Documents is",n_documents)
-    print("Number of Terms is",n_terms)
-    print("Number of Term Total Occurences is",n_occurences)
+    print("Number of Documents indexed is", index['document_count'])
+    print("Number of Terms indexed is",n_terms)
+    print("Number of Term Individual Occurences is",n_occurences)
 
-index = inverted_index('lab2_material')
+index,auxiliary_map = inverted_index('lab2_material')
 statistics(index)
+
+print(auxiliary_map)
+plt.hist(auxiliary_map)
+plt.show()
