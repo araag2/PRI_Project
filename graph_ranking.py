4639798c0b7f94ddb3129533ff79d0a0bdfd5c1b
araag2 <artur.guimas@gmail.com>
2020-11-14 19:01:21 +0000

    Refactorazing and cleaning up the code

--- graph_ranking.py
+++ graph_ranking.py
@@ -29,6 +29,8 @@ from nltk.corpus import stopwords
 from nltk import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.metrics.pairwise import manhattan_distances
 from textblob import TextBlob
 
 # File imports
@@ -36,18 +38,23 @@ from _main_ import get_files_from_directory
 from _main_ import process_collection
 from proj_utilities import *
 
-# --------------------------------------------------------------------------
-# cosine_similarity_list - Computes the cosine similarity between a document
-# and a given list of documents
+# -----------------------------------------------------------------------------------------
+# tfidf_process - Processes our entire document collection with a tf-idf vectorizer 
+# and transforms our query doc and the entire collection into tf-idf spaced vectors 
 #
-# Input: 
+# Input: doc_query - The query document that will be compared to the rest of the collection
+#        doc_dic - The entire document collection in dictionary form
+#        kwargs - Parameters for the tf-idf vectorizer
 #
-# Behaviour: 
+# Behaviour: Creates a tf-idf vectorizer and fits the entire document collection into it. 
+# Afterwards, transforms both the query document and the entire collection into vector form,
+# allowing them to be directly used to calculate similarities. It also converts structures
+# into to an easy form to manipulate at the previous higher level.
 #
-# Output: 
-# -----------------------------------------------------------------------------
-def cosine_similarity_list(doc_query, doc_dic, theta, **kwargs):
-    result = {}
+# Output: A list of document keys (ids), the query doc in vector form and the entire doc
+# collection in vector form.
+# -----------------------------------------------------------------------------------------
+def tfidf_process(doc_query, doc_dic, **kwargs):
     doc_keys = list(doc_dic.keys())
     doc_list = []
 
@@ -61,20 +68,96 @@ def cosine_similarity_list(doc_query, doc_dic, theta, **kwargs):
     doc_list_vectors = vec.transform(doc_list)
     doc_vector = vec.transform([doc_query])
 
-    similarity_list = cosine_similarity(doc_vector, doc_list_vectors)[0]
-    
-    for i in range(len(similarity_list)):
-        if similarity_list[i] >= theta:
-            result[int(doc_keys[i])] = similarity_list[i]
+    return [doc_keys, doc_vector, doc_list_vectors]
+
+
+# -----------------------------------------------------------------------------
+# manhattan_distance_dic - Computes the cosine similarity between a document
+# and a given list of documents
+#
+# Input: 
+#
+# Behaviour: 
+#
+# Output: 
+# -----------------------------------------------------------------------------
+def manhattan_distance_dic(doc_query, doc_dic, theta, **kwargs):
+    result = {}
+    tfidf_processed_list = tfidf_process(doc_query, doc_dic, **kwargs)
+
+    doc_keys = tfidf_processed_list[0]
+    doc_vector = tfidf_processed_list[1]
+    doc_list_vectors = tfidf_processed_list[2]
+
+    distance_list = manhattan_distances(doc_vector, doc_list_vectors)[0]
+
+    for i in range(len(distance_list)):
+        if distance_list[i] != 0 and 1/distance_list[i] >= theta:
+            result[int(doc_keys[i])] = 1/distance_list[i]
 
     return result
 
-# --------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# eucledian_distance_dic - Computes the cosine similarity between a document
+# and a given list of documents
+#
+# Input:  - The document collection to build our graph with
+#        sim - The similarity measure used
+#        theta - The similarity threshold 
+#        kwargs -
+#
+# Behaviour: 
+#
+# Output: 
+# -----------------------------------------------------------------------------
+def eucledian_distance_dic(doc_query, doc_dic, theta, **kwargs):
+    result = {}
+    tfidf_processed_list = tfidf_process(doc_query, doc_dic, **kwargs)
+
+    doc_keys = tfidf_processed_list[0]
+    doc_vector = tfidf_processed_list[1]
+    doc_list_vectors = tfidf_processed_list[2]
+
+    distance_list = euclidean_distances(doc_vector, doc_list_vectors)[0]
+
+    for i in range(len(distance_list)):
+        if distance_list[i] != 0 and 1/distance_list[i] >= theta:
+            result[int(doc_keys[i])] = 1/distance_list[i]
+
+    return result
+
+# -----------------------------------------------------------------------------
+# cosine_similarity_dic - Computes the cosine similarity between a document
+# and a given list of documents
+#
+# Input: 
+#
+# Behaviour: 
+#
+# Output: 
+# -----------------------------------------------------------------------------
+def cosine_similarity_dic(doc_query, doc_dic, theta, **kwargs):
+    result = {}
+    tfidf_processed_list = tfidf_process(doc_query, doc_dic, **kwargs)
+
+    doc_keys = tfidf_processed_list[0]
+    doc_vector = tfidf_processed_list[1]
+    doc_list_vectors = tfidf_processed_list[2]
+
+    distance_list = cosine_similarity(doc_vector, doc_list_vectors)[0]
+
+    for i in range(len(distance_list)):
+        if distance_list[i] >= theta:
+            result[int(doc_keys[i])] = distance_list[i]
+
+    return result
+
+# ------------------------------------------------------------------------------
 # build_graph - Builds a document graph from document collection D using
 # the similarity measure in sim agains theta threshold
 #
 # Input: D - The document collection to build our graph with
-#        sim - The similarity measure used
+#        sim - [*cosine | eucledian | manhattan] : The similarity measure used
 #        theta - The similarity threshold 
 #        kwargs -
 #
@@ -82,24 +165,37 @@ def cosine_similarity_list(doc_query, doc_dic, theta, **kwargs):
 #
 # Output: A dictionary representing the weighted undirect graph that
 # connect all documents on the basis of the given similarity measure
-# -----------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 def build_graph(D, sim, theta, **kwargs):
+    sim_methods = {'cosine': cosine_similarity_dic, 'euclidean': eucledian_distance_dic, 'manhattan': manhattan_distance_dic}
+
     #doc_dic = process_collection(D, False, **kwargs)
-    doc_dic = read_dic_from_file('test_dic')
+    doc_dic = read_from_file('test_dic')
 
     graph = {}
     for doc in doc_dic:
         graph[int(doc)] = {}
 
-    if sim == None or sim == 'cosine':
-        for doc in doc_dic:
-            doc_id = int(doc)
-            similarity_dic = cosine_similarity_list(doc_dic[doc], doc_dic, theta, **kwargs)
+    sim_method = None
 
-            for simil_doc in similarity_dic:
-                if doc_id != simil_doc:
-                    graph[doc_id][simil_doc] = similarity_dic[simil_doc]
-                    graph[simil_doc][doc_id] = similarity_dic[simil_doc]
+    if sim == None:
+        sim_method = sim_methods['cosine']
+
+    elif sim == 'cosine' or sim == 'euclidean' or sim == 'manhattan':
+        sim_method = sim_methods[sim]
+    
+    else:
+        print("Error: similarity measure not recognized.")
+        return
+
+    for doc in doc_dic:
+        doc_id = int(doc)
+        similarity_dic = sim_method(doc_dic[doc], doc_dic, theta, **kwargs)
+
+        for simil_doc in similarity_dic:
+            if doc_id != simil_doc:
+                graph[doc_id][simil_doc] = similarity_dic[simil_doc]
+                graph[simil_doc][doc_id] = similarity_dic[simil_doc]
 
     return graph
 
@@ -119,7 +215,7 @@ def undirected_page_rank(q, D, p, sim, theta, **kwargs):
 # ~ Just the Main Function ~
 # --------------------------------------------------------------------------------
 def main():
-    print(build_graph(None, 'cosine', 0.3))
+    print(build_graph(None, 'euclidean', 0.3))
     return
 
 
--- proj_utilities.py
+++ proj_utilities.py
@@ -3,18 +3,18 @@ import sys
 import pickle
 
 # --------------------------------------------------------------------------
-# write_dic_to_file() - Small auxiliary function to write a dictionary to
+# write_to_file() - Small auxiliary function to write a dictionary to
 # a file
 # -----------------------------------------------------------------------------
-def write_dic_to_file(dic, filename):
+def write_to_file(dic, filename):
     with open('saved_data/{}.txt'.format(filename), 'wb') as write_f:
         pickle.dump(dic, write_f)
     return
 
 # --------------------------------------------------------------------------
-# read_dic_from_file() - Small auxiliary function to read a dictionary from
+# read_from_file() - Small auxiliary function to read a dictionary from
 # a file
 # -----------------------------------------------------------------------------
-def read_dic_from_file(filename):
+def read_from_file(filename):
     with open('saved_data/{}.txt'.format(filename), 'rb') as read_f:
         return pickle.load(read_f) 
